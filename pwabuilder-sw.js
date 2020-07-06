02a6d8a63e6e5833dee20626682cfbb1c310f296
cagedmorpork <master.chiew@gmail.com>
2020-07-06 15:52:14 +0800

    Attempt to cache json data with service worker.

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,6 +3,12 @@
 // bigger maxEntries
 //This is the service worker with the Advanced caching
 
+// for json files, we will use a regex instead of a destination
+// a request.destination must be one of the following:
+// (https://fetch.spec.whatwg.org/#concept-request-destination)
+// empty string, "audio", "audioworklet", "document", "embed", "font", "frame", "iframe", "image", "manifest", 
+// "object", "paintworklet", "report", "script", "serviceworker", "sharedworker", "style", "track", "video", "worker", or "xslt"
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 const HTML_CACHE = "html";
@@ -10,6 +16,7 @@ const JS_CACHE = "javascript";
 const STYLE_CACHE = "stylesheets";
 const IMAGE_CACHE = "images";
 const FONT_CACHE = "fonts";
+const JSON_CACHE = "json";
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
@@ -72,6 +79,18 @@ workbox.routing.registerRoute(
         plugins: [
             new workbox.expiration.ExpirationPlugin({
                 maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.+\\.json$'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: JSON_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100,
             }),
         ],
     })
